e7df9e8ef9d3b15e4070dd7dddb49759b286c800
jagadeesh <jagadeesh88.n@gmail.com>
2023-11-26 23:58:15 +0530

    add code

---
+++ src/testingadd.java
@@ -0,0 +1,19 @@
+
+public class testingadd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a  = 20 ; 
+		int b =  30 ; 
+		
+		if(a>b) {
+			System.out.println(a);
+		} else {
+			System.out.println(b);
+		}
+
+	}
+
+}
